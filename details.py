89f5ec5d0c91ef95f47c49b8902a3bc5a3f8ae3a
Lord <110597375+kd2929@users.noreply.github.com>
2023-10-24 09:40:05 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = "24250238"
 api_hash = "cb3f118ce5553dc140127647edcf3720"
 bot_token = "6253605770:AAGPMePXJh03Z5XVZDUPpKA8sHIMY5rqsTE" 
-sudo_group = "-1001844318603"
+sudo_group = "-4052021007"
